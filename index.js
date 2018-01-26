c456710e5a43291b8642dceb4bc88c91e6afbf99
Florin Cosmin <florincosmin@fastmail.com>
2018-01-26 17:46:21 +0100

    added a new implementation

--- index.js
+++ index.js
@@ -10,6 +10,114 @@
 
   clearConsole();
 
+  /**
+   * define an array of all roman numerals
+   * aNumerals = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+   * define an array of main roman numerals
+   * mNumerals = ['I', 'X', 'C', 'M'];
+   * define an array of secondary roman numerals
+   * sNumerals = ['V', 'L', 'D'];
+   * 
+   * ====== convertToRoman algorithm ======
+   * Let's take a big number as an example -> 3999. We can write this
+   * number like so: 3999 = 3000 + 900 + 90 + 9, or better 3*1000 + 9*100 + 9*10 + 9*1
+   * and using roman numerals results that 3999 = 3*M + 9*C + 9*X + 9*I
+   * In other words: 3999 = MMM CCCCCCCCC XXXXXXXXX IIIIIIIII
+   * 
+   * We have written 3999 as a sequence of roman numerals (using only M, C, X and I)
+   * However some of these strings are incorrect (e.g. IIIIIIIII). We need
+   * to apply some rules to fix these strings.
+   * 
+   * Let's implement a function called fixMainNumeral that will fix
+   * incorrect strings according to one rule:
+   * "Don't use the same symbol more than 3 times in a row, unless the symbol is M"
+   * 
+   * Therefore the sequence CCCC or IIII are incorrect but MMMM is correct.
+   * 
+   * Now our number can be written as a sum of these correct sequences:
+   * 3999 = fixMainNumeral(3, M) + fixMainNumeral(9, C) + fixMainNumeral(9, X) + fixMainNumeral(9, I)
+   */
+
+  var aNumerals = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+  var numbersMap = {
+    1: 'I',
+    5: 'V',
+    10: 'X',
+    50: 'L',
+    100: 'C',
+    500: 'D',
+    1000: 'M'
+  };
+
+  function convertToRoman(num) {
+
+    // get number's digits
+    var digits = getDigits(num);
+    var digitsLen = digits.length;
+
+    var i, cRomNum;
+    var finalResult = '';
+
+    for (i=0; i<digitsLen; i++) {
+      cRomNum = numbersMap[Math.pow(10, digitsLen - 1 - i)];
+      finalResult = finalResult + fixMainNumeral(digits[i], cRomNum);
+    }
+
+    return finalResult;
+  }
+
+  // get digits out of number
+  function getDigits(num) {
+    return ('' + num).match(/[0-9]/g).map(function (d) {
+      return parseInt(d, 10);
+    });
+  }
+
+  function createSequence(char, times) {
+    var seq = '';
+    for (var i=0; i<times; i++) {
+      seq = seq + char;
+    }
+    return seq;
+  }
+
+  function fixMainNumeral(n, numeral) {
+
+    // get numeral position
+    var pos = aNumerals.indexOf(numeral);
+
+    if (n < 5) {
+      if (n < 4) {
+        return createSequence(numeral, n);
+      }
+
+      // check if there is a next numeral
+      if (pos + 1 >= aNumerals.length) {
+        return createSequence(numeral, n);
+      }
+      return  numeral + aNumerals[pos + 1];
+    }
+
+    if (n == 5) {
+      if (pos < aNumerals.length - 1) {
+        return aNumerals[pos + 1];
+      }
+      return createSequence(numeral, n);
+    }
+
+    if (n > 5) {
+      if(n < 9) {
+        return aNumerals[pos + 1] + createSequence(numeral, n - 5);
+      }
+
+      // check if there is a next numeral
+      if (pos + 2 >= aNumerals.length) {
+        return createSequence(numeral, n);
+      }
+      return numeral + aNumerals[pos + 2];
+    }
+  }
+
   var valuesToTest = {
     2: 'II',
     3: 'III',
@@ -40,266 +148,18 @@
     5293: 'MMMMMCCXCIII'
   };
 
-  var numerals = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
-  // var numerals = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
-  // I II III IV V VI VII VIII IX X
-  var numbersToNumerals = {
-    1: 'I',
-    5: 'V',
-    10: 'X',
-    50: 'L',
-    100: 'C',
-    500: 'D',
-    1000: 'M'
-  };
+  var val, res, testRes;
 
-  function generateNumeral(number, n) {
-    var i;
-    var res = '';
-    var n1 = 'M';
-    var n2 = 'M';
-    if(n == 'I') {
-      n1 = 'V';
-      n2 = 'X';
+  // loop through each key in valuesToTest
+  for (val in valuesToTest) {
+    // var res = convertToRomanNumeral(val);
+    // res = convert(val);
+    res = convertToRoman(val);
+    if (res == valuesToTest[val]) {
+      testRes = 'PASS'
+    } else {
+      testRes = 'FAIL'
     }
-    if (n == 'X') {
-      n1 = 'L';
-      n2 = 'C';
-    }
-    if (n == 'C') {
-      n1 = 'D';
-      n2 = 'M';
-    }
-
-    if (number < 4) {
-      for (i=0; i<number; i++) {
-        res = res + n;
-      }
-    }
-    if (number == 4) {
-      if(n == 'M') {
-        for(i=0; i<number; i++) {
-          res += n;
-        }
-      } else {
-        res = n + n1;
-      }
-    }
-    if (number == 5) {
-      if (n == 'M') {
-        for (i = 0; i<number; i++) {
-          res += n;
-        }
-      } else {
-        res = n1;
-      }
-    }
-    if (number > 5 && number < 9) {
-      if (n == 'M') {
-        for (i = 0; i < number; i++) {
-          res += n;
-        }
-      } else {
-        res = n1;
-        for (i = 5; i < number; i++) {
-          res += n;
-        }
-      }
-    }
-    if (number == 9) {
-      res = n + n2;
-    }
-
-    return res;
+    console.log(val + ' = ' + res + ' -> ' + testRes);
   }
-
-  function fixRomanNumeral(n) {
-    return n;
-  }
-
-  /**
-   * method to convert a maximum four digits number to a roman numeral
-   * for example 3999 -> MMMCMXCIX
-   * @param {int} number
-   */
-  function convert(number) {
-
-    // get number's digits. example [3, 9, 9, 9]
-    var digits = getDigits(number);
-
-    // final roman numeral array
-    var romanNum = [];
-
-    var i, j;
-
-    // loop through each digit
-    for (i=0; i<digits.length; i++) {
-      var pos = digits.length - 1 - i;
-      var tenPow = Math.pow(10, pos);
-
-      // current roman numeral
-      var cRomNum = numbersToNumerals[tenPow];
-
-      // get index of current roman numeral
-      var cRomNumIdx = numerals.indexOf(cRomNum);
-
-      // romanNum.push(generateNumeral(digits[i], cRomNum));
-
-      for(j=0; j<digits[i]; j++) {
-        romanNum.push(cRomNum);
-      }
-
-      /* var limit1 = numerals[cRomNumIdx - 1] || cRomNum;
-      var limit2 = numerals[cRomNumIdx - 2] || cRomNum;
-
-      if (digits[i] < 4) {
-        for(j=0; j<digits[i]; j++) {
-          romanNum.push(cRomNum);
-        }
-      }
-      if (digits[i] == 4) {
-        romanNum.push(cRomNum);
-        romanNum.push(limit1);
-      }
-      if (digits[i] == 5) {
-        romanNum.push(limit1);
-      }
-      if (digits[i] > 5 && digits[i] < 9) {
-        romanNum.push(limit1);
-        for(j=0; j<digits[i]-5; j++) {
-          romanNum.push(cRomNum);
-        }
-      }
-      if(digits[i] == 9) {
-        romanNum.push(cRomNum);
-        romanNum.push(limit2);
-      } */
-    }
-
-    // string roman numeral
-    var stringRomanNumeral = romanNum.join('');
-
-    // fix roman numeral string
-    return fixRomanNumeral(stringRomanNumeral);
-
-    // console.log(finalRomNum);
-    // return stringRomanNumeral;
-  }
-
-  // main roman numerals
-  var mainRomanNumerals = {
-    1: 'I',
-    10: 'X',
-    100: 'C',
-    1000: 'M'
-  };
-
-  // secondary roman numerals
-  var secRomanNumerals = {
-    5: 'V',
-    50: 'L',
-    500: 'D'
-  };
-
-  // get digits out of number
-  function getDigits(num) {
-    return ('' + num).match(/[0-9]/g).map(function(d) {
-      return parseInt(d, 10);
-    });
-    // .reverse();
-  }
-
-  function addNumeralOld(src, num, x) {
-    var i;
-    for (i=0; i<x; i++) {
-      src.push(num);
-    }
-    return src;
-  }
-
-  /**
-   * method to convert a maximum four digits number to a roman numeral
-   * for example 3999 -> MMMCMXCIX
-   * @param {int} number
-   */
-  function convertToRomanNumeral(number) {
-
-    // get number's digits
-    var digits = getDigits(number);
-    console.log(digits);
-
-    // final roman numeral array
-    var finalRomNum = [];
-
-    // start looping through the digits from this position
-    var i;
-
-    // loop through each digit and build the result
-    for (i=0; i<digits.length; i++) {
-      /**
-       * get the roman value for the current digit
-       * for example if i=0 -> roman value = Math.pow(10, digits.length - i)
-       */
-      var pos = digits.length - 1 - i;
-
-      // current main numeral value. can be one of ['I', 'X', 'C', 'M']
-      // var cMainNumeral = romanNumerals[Math.pow(10, pos)];
-
-      var tenPow = Math.pow(10, pos);
-
-      if (digits[i] < 5) {
-
-        /**
-         * add to the final roman numeral the current roman numeral repeated
-         * the digit itself tels how many times the numeral should be repeated
-         */
-        addNumeral(finalRomNum, mainRomanNumerals[tenPow], digits[i]);
-      } else if (digits[i] == 5) {
-
-        // add corresponding secondary roman numeral
-        addNumeral(finalRomNum, secRomanNumerals[5 * tenPow], 1);
-      } else {
-
-        // 90 = 50 + 40
-
-        // add corresponding secondary roman numeral
-        addNumeral(finalRomNum, secRomanNumerals[5 * tenPow], 1);
-
-        // log
-        // console.log('ten pow:', tenPow);
-
-        // calculate remaining number
-        var remainingNumber = (digits[i] - 5) * tenPow;
-
-        finalRomNum.push(
-          convertToRomanNumeral(remainingNumber)
-        );
-      }
-    }
-
-    // console.log(finalRomNum);
-    return finalRomNum.join('');
-  }
-
-  // test function
-  function test(testObj) {
-    var val, res, testRes;
-
-    // test values
-    for (val in testObj) {
-      if (valuesToTest.hasOwnProperty(val)) {
-        // res = convertToRomanNumeral(val);
-        res = convert(val);
-        if (res == testObj[val]) {
-          testRes = 'PASS'
-        } else {
-          testRes = 'FAIL'
-        }
-        console.log(val + ' = ' + res + ' -> ' + testRes);
-      }
-    }
-  }
-
-  // test values
-  test(valuesToTest);
 })();
